c38636b057aef6792f928283512728095859e179
sohara <svohara@gmail.com>
2013-10-03 09:35:37 -0600

    Updated evaluation/WACV2013/ANN_ErrorCorrelations.py module with additional scripts.

--- evaluation/WACV2013/ANN_ErrorCorrelations.py
+++ evaluation/WACV2013/ANN_ErrorCorrelations.py
@@ -119,6 +119,38 @@ def KDTree_ErrorCor(galleryL, probesL, ground_truth, K=3, numTrees=10, binary_sc
     ErrM = sp.hstack(errs)
     return ErrM
 
+def KDTForest_ErrorCor(galleryL, probesL, ground_truth, K=3, forest_size=6,
+                       numForests=5, binary_score=True):
+    Np = len(probesL)
+    gx = sp.array(galleryL)
+    px = sp.array(probesL)
+    
+    ground_truth = sp.reshape(ground_truth,(Np,K)) if K==1 else ground_truth
+    
+    forests = []
+    print "Building %d separate KDT Forests from input data..."%numForests
+    for _idx in range(numForests):
+        f = pyf.FLANN()
+        _params = f.build_index(gx,algorithm='kdtree', trees=forest_size)
+        forests.append(f)
+    
+    errs = []
+    
+    print "Testing %d Probe Points across %d KDT Forestsa"%(Np, numForests)
+    for f in forests:
+        print ".",
+        sys.stdout.flush()
+        [res, _ds] = f.nn_index(px, K)
+        if K==1:
+            res = sp.reshape(res,(Np,1))
+        err_vec = compute_errors(res, ground_truth, binary_score=binary_score)
+        errs.append( sp.reshape(err_vec, (Np,1) ) )
+    print ""
+      
+    ErrM = sp.hstack(errs)
+    return ErrM
+
+
 def ProxTree_ErrorCor(galleryL, probesL, ground_truth, K=3, numTrees=10, binary_score=True):
     '''
     Compute the correlation in errors between different Proximity Trees indexed
@@ -160,6 +192,44 @@ def ProxTree_ErrorCor(galleryL, probesL, ground_truth, K=3, numTrees=10, binary_
                 sys.stdout.flush()
                 
             res = ptree.getKNearest( q, K=K)
+            idxs = [idx for (_,_,idx) in res]
+            Res[i,:] = idxs
+      
+        err_vec = compute_errors(Res, ground_truth, binary_score=binary_score)
+        errs.append( sp.reshape(err_vec, (Np,1) ) )
+
+    print ""
+      
+    ErrM = sp.hstack(errs)
+    return ErrM
+
+def ProxForest_ErrorCor(galleryL, probesL, ground_truth, K=3, 
+                        forest_size=6, numForests=5, binary_score=True):
+    Np = len(probesL)
+    #gx = sp.array(galleryL)
+    #px = sp.array(probesL)
+    ground_truth = sp.reshape(ground_truth,(Np,K)) if K==1 else ground_truth
+    
+    forests = []
+    print "Building %d separate Proximity Forests from input data..."%numForests
+    for _idx in range(numForests):
+        f = pf.ProximityForest(N=forest_size, dist_func=dist_euclidean_squared, Tau=15)
+        forests.append(f)
+        f.addList(galleryL, labels=range(len(galleryL)))
+    
+    errs = []
+    
+    print "Testing %d Probe Points across %d Proximity Forests"%(Np, numForests)  
+    Res = sp.zeros((Np,K))  
+    for idx,pforest in enumerate(forests):
+        print "\nForest %d..."%(idx+1)
+        
+        for i,q in enumerate(probesL):
+            if i%100 == 0:
+                print ".",
+                sys.stdout.flush()
+                
+            res = pforest.getKNearest(q, K=K)
             idxs = [idx for (_,_,idx) in res]
             Res[i,:] = idxs
       
@@ -222,6 +292,62 @@ def script1(data=None, K=1, numTrees=20):
     
     return (errM_KD, errM_PF)
     
+def script2(data=None, K=3, forest_size=6, numForests=5):
+    '''
+    Script to compute how correlated errors are between
+    different forests (KDT,PF). A forest of KD-Trees and a
+    Proximity Forest are constructed that have approximately
+    equal predictive power (this happens on SIFT data with 3NN
+    queries at about 6 trees). Errors are correlated between
+    successive forests (of same size), to determine if
+    proximity forests have more diversity than KDT Forests.
+    @param data: A tuple (g,p) where g is a list of gallery
+    points and p is a list of probe points. If set to None,
+    then the Sift10K data will be used.
+    @param K: How many neighbors to return for each probe?
+    @param forest_size: The forest size to use when testing
+    error correlation between forests.
+    @return: Tuple (Err_KD, Err_PF) where Err_KD/PF are
+    the matrices of errors, nxp, where n is the number of
+    query points and p is the number of trees.
+    '''
+    if data is None:
+        (g,p) = loadSift10K()
+    else:
+        (g,p) = data
+        
+    resT, _ = exact_knn(g, p, K=K)
+    
+    print "===================================="
+    print "Computing errors using KD-Trees"
+    print "===================================="
+    errM_KD = KDTForest_ErrorCor(g, p, resT, K=K, forest_size=forest_size, numForests=numForests, binary_score=True)
+    print ""
+    print "===================================="
+    print "Computing errors using Proximity Trees"
+    print "===================================="
+    errM_PF = ProxForest_ErrorCor(g, p, resT, K=K, forest_size=forest_size, numForests=numForests, binary_score=True)
+    print ""
+    print "===================================="
+    print "Error correlations: KD-Trees"
+    print "===================================="
+    rc1 = avg_corr(errM_KD)
+    print "===================================="
+    print "Error correlations: Proximity Trees"
+    print "===================================="
+    rc2 = avg_corr(errM_PF)
+    print ""
+    print "===================================="
+    print "Computing error entropy..."
+    print "===================================="
+    X_KD = sp.sum(errM_KD, axis=1)
+    X_PF = sp.sum(errM_PF, axis=1)    
+    H_KD = entropy(X_KD[X_KD>0])
+    H_PF = entropy(X_PF[X_PF>0])
+    print "Entropy of KD_Tree Errors: %3.3f"%H_KD
+    print "Entropy of Prox Tree Errors: %3.3f"%H_PF
+    
+    return (errM_KD, errM_PF)
 
 if __name__ == '__main__':
     pass
